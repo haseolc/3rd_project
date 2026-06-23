1e3295a842cca3da9f6699fc3bf8c2507d8e98e0
haseolc <haseok612@naver.com>
2026-06-23 21:09:19 +0900

    feat: add EC2 role for External Secrets

--- terraform-security/iam-external-secrets.tf
+++ terraform-security/iam-external-secrets.tf
@@ -53,3 +53,69 @@ resource "aws_iam_policy" "external_secrets_read" {
     ManagedBy   = "terraform"
   }
 }
+
+############################################################
+# EC2 Assume Role Policy for External Secrets Operator
+############################################################
+
+data "aws_iam_policy_document" "external_secrets_ec2_assume_role" {
+  statement {
+    sid     = "AllowEC2AssumeRole"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+############################################################
+# IAM Role for External Secrets Operator EC2 Node
+############################################################
+
+resource "aws_iam_role" "external_secrets_ec2" {
+  name        = "3rd-project-external-secrets-ec2"
+  description = "EC2 role used by External Secrets Operator"
+  path        = "/3rd-project/"
+
+  assume_role_policy = data.aws_iam_policy_document.external_secrets_ec2_assume_role.json
+
+  tags = {
+    Name        = "3rd-project-external-secrets-ec2"
+    Project     = "3rd-project"
+    Environment = "sandbox"
+    Service     = "external-secrets"
+    ManagedBy   = "terraform"
+  }
+}
+
+
+############################################################
+# Attach Least-Privilege Policy to EC2 Role
+############################################################
+
+resource "aws_iam_role_policy_attachment" "external_secrets_read" {
+  role       = aws_iam_role.external_secrets_ec2.name
+  policy_arn = aws_iam_policy.external_secrets_read.arn
+}
+
+
+############################################################
+# EC2 Instance Profile
+############################################################
+
+resource "aws_iam_instance_profile" "external_secrets_ec2" {
+  name = "3rd-project-external-secrets-ec2"
+  path = "/3rd-project/"
+  role = aws_iam_role.external_secrets_ec2.name
+
+  tags = {
+    Name        = "3rd-project-external-secrets-ec2"
+    Project     = "3rd-project"
+    Environment = "sandbox"
+    Service     = "external-secrets"
+    ManagedBy   = "terraform"
+  }
+}
